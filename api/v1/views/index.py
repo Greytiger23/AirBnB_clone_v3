9e75f001cbe9367dd025d19914d1b1dd79a4f139
Greytiger23 <isabelmothoa49@gmail.com>
2024-04-27 20:32:38 -0700

    airbnb clone v3

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" index file"""
+
+
+from flask import jsonify
+from . import app_views
+
+
+@app.views.route('/status', methods=['GET'])
+def status():
+    """displays the status"""
+    return jsonify({"status": "OK"})
