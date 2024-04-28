175fedcdcb5ab38f49ab1e4f2272257965a1be8a
Greytiger23 <isabelmothoa49@gmail.com>
2024-04-28 15:10:07 -0700

    airbnb clone v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,18 +3,26 @@
 
 
 from flask import jsonify
-from . import app_views
-from models.engine.db_storage import DBStorage
+from models import storage
+from models.engine.db_storage import classes
+from api.v1.views import app_views
 
-storage = DBStorage()
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """displays the status"""
+    return jsonify({"status": "OK"})
+
+# register the /status route thr app_view object
+app_views.add_url_rule('/status', 'status', status)
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
+def get_stats():
     """displays the status"""
     stats = {}
-    for obj_type in ['Amenity', 'City', 'Place', 'Review', 'State', 'Users']:
+    for obj_type in ['Amenity', 'City', 'Place', 'Review', 'State', 'User']:
         stats[obj_type.lower()] = storage.count(classes[obj_type])
     return jsonify(stats)
 
 # register the /stats route to the app_views object
-app_views.add_url_rule('/stats', 'stats', stats)
+app_views.add_url_rule('/stats', 'stats', get_stats)
