1d2baec5a7a2fa7b9a91e29020aea12d9f378211
Greytiger23 <isabelmothoa49@gmail.com>
2024-04-28 11:00:55 -0700

    airbnb clone v3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ int it file"""
 
-"""
+
 from flask import Blueprint
-from api.v1.views.index import *
 
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')"""
+from .index import *
+
+#import app_views after defining it
+from api.v1.views import app_views
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ index file"""
 
-"""
+
 from flask import jsonify
 from . import app_views
 
 
-@app.views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """displays the status"""
-    return jsonify({"status": "OK"})"""
+    return jsonify({'status': 'OK'})
+
+# register the /status route to the app_views object
+app_views.add_url_rule('/status', 'status', status)
