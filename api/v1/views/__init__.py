aca3f27967661a86411dc3832d40f4aad1662cac
Greytiger23 <isabelmothoa49@gmail.com>
2024-04-29 12:31:41 -0700

    airbnb clone v3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 CORS(app_views)
 
+
 # import app_views
 from api.v1.views.index import *
 from api.v1.views.states import *
