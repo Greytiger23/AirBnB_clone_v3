d2847542d19957f6e298714d921cda59a8feb8d3
Greytiger23 <isabelmothoa49@gmail.com>
2024-04-29 16:00:18 -0700

    airbnb clone v3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,12 +4,14 @@
 
 from flask import Blueprint
 from flask_cors import CORS
+from api.v1.views.index import *
+from api.v1.views.states import states_blueprint
+from api.v1.views.cities import cities_blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+states_blueprint = Blueprint('states', __name__)
+cities_bluerint = Blueprint('cities', __name__)
 CORS(app_views)
 
-
-# import app_views
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
+app_views.register_blueprint(states_blueprint)
+app_views.register_blueprint(cities_blueprint)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,12 +3,11 @@
 
 
 from flask import jsonify, request, abort
-from models import storage
 from models.state import State
-from api.v1.views import app_views
+from models.engine.db_storage import DBStorage
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@states_blueprint.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """get the states"""
     states = []
@@ -17,7 +16,8 @@ def get_states():
     return jsonify(states)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@states_blueprint.route(
+        '/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """get the state"""
     state = storage.get(State, state_id)
@@ -26,7 +26,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route(
+@states_blueprint.route(
         '/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """delete state"""
@@ -38,20 +38,22 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@states_blueprint.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """create state"""
     if not request.get_json():
         abort(400, description="Not a JSON")
     if 'name' not in request.get_json():
         abort(400, description="Misssing name")
-    state = State(**request.get_json())
+    state = State()
+    state.name = request.get_json()['name']
     storage.new(state)
     storage.save()
     return jsonify(state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@states_blueprint.route(
+        '/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """updates the state"""
     state = storage.get(State, state_id)
