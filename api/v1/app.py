598db103304d6aac4f1514e9d9b314b37c6a34e5
Greytiger23 <isabelmothoa49@gmail.com>
2024-04-30 19:35:40 -0700

    airbnb clone v3 fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,16 +4,8 @@
 
 import os
 from flask import Flask, jsonify
-from api.v1.views import app_views
-from api.v1.views.states import states
-from api.v1.views.cities import cities
+from api.v1 import app
 from models import storage
-
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-app.register_blueprint(states)
-app.register_blueprint(cities)
 
 
 @app.teardown_appcontext
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,8 +6,12 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
+from api.v1.views.states import states
+from api.v1.views.cities import cities
 
 from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.register_blueprint(states)
+app.register_blueprint(cities)
