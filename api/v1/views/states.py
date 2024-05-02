68947b04b6c1ca58379b3cd01114edbbd9e4c16d
Greytiger23 <isabelmothoa49@gmail.com>
2024-05-01 18:34:55 -0700

    airbnb clone v3 fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,13 +7,11 @@ from models.state import State
 from models import storage
 from api.v1.views import app_views
 
-
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """get the states"""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
-
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
@@ -23,20 +21,18 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """create state"""
     if not request.json:
-        abort(400, 'Not a JSON')
+        abort(400, "Not a JSON")
     if 'name' not in request.json:
-        abort(400, 'Misssing name')
-    data = request.get_json()
+        abort(400, "Misssing name")
+    data = request.json()
     state = State(**data)
     storage.new(state)
     storage.save()
     return jsonify(state.to_dict()), 201
-
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
@@ -45,7 +41,7 @@ def update_state(state_id):
     if state is None:
         abort(404)
     if not request.json:
-        abort(400, 'Not a JSON')
+        abort(400, "Not a JSON")
     data = request.get_json()
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
@@ -53,9 +49,7 @@ def update_state(state_id):
     storage.save()
     return jsonify(state.to_dict()), 200
 
-
-@app_views.route(
-        '/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """delete state"""
     state = storage.get(State, state_id)
