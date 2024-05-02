4885c5d3aadab3f2b488000a5dcde47068160c45
Greytiger23 <isabelmothoa49@gmail.com>
2024-05-02 11:29:25 -0700

    airbnb clone v3

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -39,14 +39,13 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """create state"""
-    if not request.get_json():
-        abort(400, 'Not a JSON')
-    if 'name' not in request.get_json():
-        abort(400, 'Misssing name')
     data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Misssing name')
     state = State(**data)
-    storage.new(state)
-    storage.save()
+    state.save()
     return jsonify(state.to_dict()), 201
 
 
@@ -56,11 +55,12 @@ def update_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if not request.get_json():
-        abort(400, 'Not a JSON')
     data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    ignore_keys = ['id', 'created_at', 'updated_at']
     for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ignore_keys:
             setattr(state, key, value)
-    storage.save()
+    state.save()
     return jsonify(state.to_dict()), 200
