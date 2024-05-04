9eb7eb00a23a5f5eaacfae9192bdbc180d2b7175
Greytiger23 <isabelmothoa49@gmail.com>
2024-05-03 18:50:31 -0700

    airbnb clone v3

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -10,8 +10,7 @@ from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/cities/<city_id>/places',
-                 methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', strict_slashes=False)
 def get_places(city_id):
     """retrieves the list of all places"""
     city = storage.get(City, city_id)
@@ -21,7 +20,7 @@ def get_places(city_id):
     return jsonify(places)
 
 
-@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>', strict_slashes=False)
 def get_place(place_id):
     """retrieves a place object"""
     place = storage.get(Place, place_id)
@@ -45,19 +44,21 @@ def delete_place(place_id):
                  methods=['POST'], strict_slashes=False)
 def create_place(city_id):
     """creates a place"""
+    if request.content_type != 'application/json':
+        return abort(400, 'Not a JSON')
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    if not request.get_json():
-        abort(400, 'Not a JSON')
     data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
     if 'user_id' not in data:
         abort(400, 'Missing user_id')
-    if 'name' not in data:
-        abort(400, 'Missing name')
     user = storage.get(User, data['user_id'])
     if user is None:
         abort(404)
+    if 'name' not in data:
+        abort(400, 'Missing name')
     data['city_id'] = city_id
     place = Place(**data)
     place.save()
@@ -67,12 +68,14 @@ def create_place(city_id):
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     """updates a place"""
+    if request.content_type != 'application/json':
+        return abort(400, 'Not a JSON')
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    if not request.get_json():
-        abort(400, 'Not a JSON')
     data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
     for key, value in data.items():
         if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
             setattr(place, key, value)
